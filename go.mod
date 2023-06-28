e171d2ead68c106abdfe865f7392aafb7d20110a
Amirsalarsafaei <amirsalar.safaei@divar.ir>
2023-06-28 16:30:15 +0330

    intialized

--- go.mod
+++ go.mod
@@ -1,20 +1,5 @@
-module github.com/incu6us/goimports-reviser/v3
+module github.com/amirsalarsafaei/gencode-remover
 
 go 1.18
 
-require (
-	github.com/stretchr/testify v1.6.1
-	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e
-	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3
-	golang.org/x/tools v0.1.10
-)
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
-)
+require golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e
--- main.go
+++ main.go
@@ -1,39 +1,18 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"os/user"
-	"path"
-	"path/filepath"
 	"strings"
 
-	"github.com/incu6us/goimports-reviser/v3/helper"
-	"github.com/incu6us/goimports-reviser/v3/reviser"
+	"github.com/amirsalarsafaei/gencode-remover/remover"
 )
 
 const (
-	projectNameArg         = "project-name"
-	versionArg             = "version"
-	removeUnusedImportsArg = "rm-unused"
-	setAliasArg            = "set-alias"
-	companyPkgPrefixesArg  = "company-prefixes"
-	outputArg              = "output"
-	importsOrderArg        = "imports-order"
-	formatArg              = "format"
-	listDiffFileNameArg    = "list-diff"
-	setExitStatusArg       = "set-exit-status"
-	recursiveArg           = "recursive"
-	useCacheArg            = "use-cache"
-	applyToGeneratedFiles  = "apply-to-generated-files"
-
-	// Deprecated options
-	localArg    = "local"
-	filePathArg = "file-path"
+	versionArg   = "version"
+	recursiveArg = "recursive"
 )
 
 // Project build specific vars
@@ -43,119 +22,15 @@ var (
 	SourceURL string
 	GoVersion string
 
-	shouldShowVersion           *bool
-	shouldRemoveUnusedImports   *bool
-	shouldSetAlias              *bool
-	shouldFormat                *bool
-	shouldApplyToGeneratedFiles *bool
-	listFileName                *bool
-	setExitStatus               *bool
-	isRecursive                 *bool
-	isUseCache                  *bool
-)
-
-var (
-	projectName, companyPkgPrefixes, output, importsOrder string
-
-	// Deprecated
-	localPkgPrefixes, filePath string
+	shouldShowVersion *bool
+	isRecursive       *bool
 )
 
 func init() {
-	flag.StringVar(
-		&filePath,
-		filePathArg,
-		"",
-		"Deprecated. Put file name as an argument(last item) of command line.",
-	)
-
-	flag.StringVar(
-		&projectName,
-		projectNameArg,
-		"",
-		"Your project name(ex.: github.com/incu6us/goimports-reviser). Optional parameter.",
-	)
-
-	flag.StringVar(
-		&companyPkgPrefixes,
-		companyPkgPrefixesArg,
-		"",
-		"Company package prefixes which will be placed after 3rd-party group by default(if defined). Values should be comma-separated. Optional parameters.",
-	)
-
-	flag.StringVar(
-		&localPkgPrefixes,
-		localArg,
-		"",
-		"Deprecated",
-	)
-
-	flag.StringVar(
-		&output,
-		outputArg,
-		"file",
-		`Can be "file", "write" or "stdout". Whether to write the formatted content back to the file or to stdout. When "write" together with "-list-diff" will list the file name and write back to the file. Optional parameter.`,
-	)
-
-	flag.StringVar(
-		&importsOrder,
-		importsOrderArg,
-		"std,general,company,project",
-		`Your imports groups can be sorted in your way. 
-std - std import group; 
-general - libs for general purpose; 
-company - inter-org or your company libs(if you set '-company-prefixes'-option, then 4th group will be split separately. In other case, it will be the part of general purpose libs); 
-project - your local project dependencies. 
-Optional parameter.`,
-	)
-
-	listFileName = flag.Bool(
-		listDiffFileNameArg,
-		false,
-		"Option will list files whose formatting differs from goimports-reviser. Optional parameter.",
-	)
-
-	setExitStatus = flag.Bool(
-		setExitStatusArg,
-		false,
-		"set the exit status to 1 if a change is needed/made. Optional parameter.",
-	)
-
-	shouldRemoveUnusedImports = flag.Bool(
-		removeUnusedImportsArg,
-		false,
-		"Remove unused imports. Optional parameter.",
-	)
-
-	shouldSetAlias = flag.Bool(
-		setAliasArg,
-		false,
-		"Set alias for versioned package names, like 'github.com/go-pg/pg/v9'. "+
-			"In this case import will be set as 'pg \"github.com/go-pg/pg/v9\"'. Optional parameter.",
-	)
-
-	shouldFormat = flag.Bool(
-		formatArg,
-		false,
-		"Option will perform additional formatting. Optional parameter.",
-	)
-
 	isRecursive = flag.Bool(
 		recursiveArg,
 		false,
 		"Apply rules recursively if target is a directory. In case of ./... execution will be recursively applied by default. Optional parameter.",
-	)
-
-	isUseCache = flag.Bool(
-		useCacheArg,
-		false,
-		"Use cache to improve performance. Optional parameter.",
-	)
-
-	shouldApplyToGeneratedFiles = flag.Bool(
-		applyToGeneratedFiles,
-		false,
-		"Apply imports sorting and formatting(if the option is set) to generated files. Generated file is a file with first comment which starts with comment '// Code generated'. Optional parameter.",
 	)
 
 	if Tag != "" {
@@ -187,7 +62,6 @@ func printVersion() {
 }
 
 func main() {
-	deprecatedMessagesCh := make(chan string, 10)
 	flag.Parse()
 
 	if shouldShowVersion != nil && *shouldShowVersion {
@@ -196,10 +70,6 @@ func main() {
 	}
 
 	originPath := flag.Arg(0)
-	if filePath != "" {
-		deprecatedMessagesCh <- fmt.Sprintf("-%s is deprecated. Put file name as last argument to the command(Example: goimports-reviser -rm-unused -set-alias -format goimports-reviser/main.go)", filePathArg)
-		originPath = filePath
-	}
 
 	if originPath == "" {
 		originPath = reviser.StandardInput
@@ -211,156 +81,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	var options reviser.SourceFileOptions
-	if shouldRemoveUnusedImports != nil && *shouldRemoveUnusedImports {
-		options = append(options, reviser.WithRemovingUnusedImports)
-	}
-
-	if shouldSetAlias != nil && *shouldSetAlias {
-		options = append(options, reviser.WithUsingAliasForVersionSuffix)
-	}
-
-	if shouldFormat != nil && *shouldFormat {
-		options = append(options, reviser.WithCodeFormatting)
-	}
-
-	if shouldApplyToGeneratedFiles == nil || !*shouldApplyToGeneratedFiles {
-		options = append(options, reviser.WithSkipGeneratedFile)
-	}
-
-	if localPkgPrefixes != "" {
-		if companyPkgPrefixes != "" {
-			companyPkgPrefixes = localPkgPrefixes
-		}
-		deprecatedMessagesCh <- fmt.Sprintf(`-%s is deprecated and will be removed soon. Use -%s instead.`, localArg, companyPkgPrefixesArg)
-	}
-
-	if companyPkgPrefixes != "" {
-		options = append(options, reviser.WithCompanyPackagePrefixes(companyPkgPrefixes))
-	}
-
-	if importsOrder != "" {
-		order, err := reviser.StringToImportsOrders(importsOrder)
-		if err != nil {
-			fmt.Printf("%s\n\n", err)
-			printUsage()
-			os.Exit(1)
-		}
-		options = append(options, reviser.WithImportsOrder(order))
-	}
-
-	originProjectName, err := helper.DetermineProjectName(projectName, originPath)
-	if err != nil {
-		fmt.Printf("%s\n\n", err)
-		printUsage()
-		os.Exit(1)
-	}
-
-	close(deprecatedMessagesCh)
-
 	if _, ok := reviser.IsDir(originPath); ok {
-		err := reviser.NewSourceDir(originProjectName, originPath, *isRecursive).Fix(options...)
+		err := reviser.NewSourceDir(originPath, *isRecursive).DeleteAutoGeneratedFiles()
 		if err != nil {
 			log.Fatalf("Failed to fix directory: %+v\n", err)
 		}
 		return
 	}
-
-	originPath, err = filepath.Abs(originPath)
-	if err != nil {
-		log.Fatalf("Failed to get abs path: %+v\n", err)
-	}
-
-	var formattedOutput []byte
-	var hasChange bool
-	if *isUseCache {
-		hash := md5.Sum([]byte(originPath))
-
-		u, err := user.Current()
-		if err != nil {
-			log.Fatalf("Failed to get current user: %+v\n", err)
-		}
-		cacheDir := path.Join(u.HomeDir, ".cache", "goimports-reviser")
-		if err = os.MkdirAll(cacheDir, os.ModePerm); err != nil {
-			log.Fatalf("Failed to create cache directory: %+v\n", err)
-		}
-		cacheFile := path.Join(cacheDir, hex.EncodeToString(hash[:]))
-
-		var cacheContent, fileContent []byte
-		if cacheContent, err = os.ReadFile(cacheFile); err == nil {
-			// compare file content hash
-			var fileHashHex string
-			if fileContent, err = os.ReadFile(originPath); err == nil {
-				fileHash := md5.Sum(fileContent)
-				fileHashHex = hex.EncodeToString(fileHash[:])
-			}
-			if string(cacheContent) == fileHashHex {
-				// point to cache
-				return
-			}
-		}
-		formattedOutput, hasChange, err = reviser.NewSourceFile(originProjectName, originPath).Fix(options...)
-		if err != nil {
-			log.Fatalf("Failed to fix file: %+v\n", err)
-		}
-		fileHash := md5.Sum(formattedOutput)
-		fileHashHex := hex.EncodeToString(fileHash[:])
-		if fileInfo, err := os.Stat(cacheFile); err != nil || fileInfo.IsDir() {
-			if _, err = os.Create(cacheFile); err != nil {
-				log.Fatalf("Failed to create cache file: %+v\n", err)
-			}
-		}
-		file, _ := os.OpenFile(cacheFile, os.O_RDWR, os.ModePerm)
-		defer file.Close()
-		if err = file.Truncate(0); err != nil {
-			log.Fatalf("Failed file truncate: %+v\n", err)
-		}
-		if _, err = file.Seek(0, 0); err != nil {
-			log.Fatalf("Failed file seek: %+v\n", err)
-		}
-		if _, err = file.WriteString(fileHashHex); err != nil {
-			log.Fatalf("Failed to write file hash: %+v\n", err)
-		}
-	} else {
-		formattedOutput, hasChange, err = reviser.NewSourceFile(originProjectName, originPath).Fix(options...)
-		if err != nil {
-			log.Fatalf("Failed to fix file: %+v\n", err)
-		}
-	}
-
-	resultPostProcess(hasChange, deprecatedMessagesCh, originPath, formattedOutput)
-}
-
-func resultPostProcess(hasChange bool, deprecatedMessagesCh chan string, originFilePath string, formattedOutput []byte) {
-	if !hasChange && *listFileName {
-		printDeprecations(deprecatedMessagesCh)
-		return
-	}
-	if hasChange && *listFileName && output != "write" {
-		fmt.Println(originFilePath)
-	} else if output == "stdout" || originFilePath == reviser.StandardInput {
-		fmt.Print(string(formattedOutput))
-	} else if output == "file" || output == "write" {
-		if !hasChange {
-			printDeprecations(deprecatedMessagesCh)
-			return
-		}
-
-		if err := os.WriteFile(originFilePath, formattedOutput, 0644); err != nil {
-			log.Fatalf("failed to write fixed result to file(%s): %+v\n", originFilePath, err)
-		}
-		if *listFileName {
-			fmt.Println(originFilePath)
-		}
-	} else {
-		log.Fatalf(`invalid output "%s" specified`, output)
-	}
-
-	if hasChange && *setExitStatus {
-		os.Exit(1)
-	}
-
-	printDeprecations(deprecatedMessagesCh)
 }
 
 func validateRequiredParam(filePath string) error {
@@ -368,20 +95,8 @@ func validateRequiredParam(filePath string) error {
 		stat, _ := os.Stdin.Stat()
 		if stat.Mode()&os.ModeNamedPipe == 0 {
 			// no data on stdin
-			return fmt.Errorf("-%s should be set", filePathArg)
+			return fmt.Errorf("please give file dir path")
 		}
 	}
 	return nil
-}
-
-func printDeprecations(deprecatedMessagesCh chan string) {
-	var hasDeprecations bool
-	for deprecatedMessage := range deprecatedMessagesCh {
-		hasDeprecations = true
-		fmt.Printf("%s\n", deprecatedMessage)
-	}
-	if hasDeprecations {
-		fmt.Printf("All changes to file are applied, but command-line syntax should be fixed\n")
-		os.Exit(1)
-	}
 }
